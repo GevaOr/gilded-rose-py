92706f8eec7cf5043a017bbbe76addf68c56b5fc
Geva <70754020+GevaOr@users.noreply.github.com>
2021-06-16 00:25:22 +0300

    everything is working but the backstage passes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,51 +4,115 @@ class GildedRose(object):
     AGED_BRIE = "Aged Brie"
     BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
     SULFURAS = "Sulfuras, Hand of Ragnaros"
-    CONJURED_PREFIX = "Conjured"
 
     SPECIAL_CASES = [
         AGED_BRIE,
         BACKSTAGE_PASSES,
         SULFURAS,
-        CONJURED_PREFIX
     ]
+
+    CONJURED_PREFIX = "Conjured"
 
     def __init__(self, items):
         self.items = items
 
+    def _decrease_item_quality_by(self, item, decrease_by):
+        outcome = item.quality - decrease_by
+        if outcome >= 0:
+            item.quality = outcome
+            return
+        item.quality = 0
+
+    # def _increase_item_quality_by(self, item, increase_by):
+    #     outcome = item.quality + increase_by
+    #     if outcome <= 50:
+    #         item.quality = outcome
+    #         return
+    #     item.quality = 50
+
+    def _decrease_item_sell_in_by(self, item, decrease_by):
+        item.sell_in -= decrease_by
+
+    def _is_item_special_case(self, item):
+        if item.name in self.SPECIAL_CASES or item.name.startswith(self.CONJURED_PREFIX):
+            return True
+        return False
+
+    def _is_item_expired(self, item):
+        return item.sell_in < 0
+
+    def _is_item_max_quality(self, item):
+        return item.quality >= 50
+
+    def _update_backstage_pass_decrease(self, item):
+        if item.sell_in < 0:
+            item.quality = 0
+            return
+        quality_increase = 1
+        if item.sell_in <= 10:
+            quality_increase += 1
+        if item.sell_in < 6:
+            quality_increase += 1
+        item.quality += quality_increase
+
     def update_quality(self):
         for item in self.items:
-            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != self.SULFURAS:
-                        item.quality -= 1
-                    if item.name.startswith(self.CONJURED_PREFIX):
-                        item.quality -= 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+            time_passed = 1
+            quality_decrease_amount = 1
+            if self._is_item_special_case(item):
+                if item.name == self.SULFURAS:
+                    time_passed = 0
+                    quality_decrease_amount = 0
+                self._decrease_item_sell_in_by(item, time_passed)
+                if item.name.startswith(self.CONJURED_PREFIX):
+                    quality_decrease_amount *= 2
+                elif not self._is_item_max_quality(item):
+                    quality_decrease_amount = -1
                     if item.name == self.BACKSTAGE_PASSES:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != self.SULFURAS:
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name != self.AGED_BRIE:
-                    if item.name != self.BACKSTAGE_PASSES:
-                        if item.quality > 0:
-                            if item.name != self.SULFURAS:
-                                if item.name.startswith(self.CONJURED_PREFIX):
-                                    item.quality -= 1
-                                item.quality -= 1
-                    else:
-                        item.quality = item.quality - item.quality
+                        print("BOOM")
+                        self._update_backstage_pass_decrease(
+                            item)
+                        quality_decrease_amount = 0
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    quality_decrease_amount = 0
+            if self._is_item_expired(item):
+                quality_decrease_amount *= 2
+
+            self._decrease_item_quality_by(item, quality_decrease_amount)
+
+    # def update_quality(self):
+    #     for item in self.items:
+    #         if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
+    #             if item.quality > 0:
+    #                 if item.name != self.SULFURAS:
+    #                     item.quality -= 1
+    #                 if item.name.startswith(self.CONJURED_PREFIX):
+    #                     item.quality -= 1
+    #         else:
+    #             if item.quality < 50:
+    #                 item.quality = item.quality + 1
+    #                 if item.name == self.BACKSTAGE_PASSES:
+    #                     if item.sell_in < 11:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #                     if item.sell_in < 6:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #         if item.name != self.SULFURAS:
+    #             item.sell_in -= 1
+    #         if item.sell_in < 0:
+    #             if item.name != self.AGED_BRIE:
+    #                 if item.name != self.BACKSTAGE_PASSES:
+    #                     if item.quality > 0:
+    #                         if item.name != self.SULFURAS:
+    #                             if item.name.startswith(self.CONJURED_PREFIX):
+    #                                 item.quality -= 1
+    #                             item.quality -= 1
+    #                 else:
+    #                     item.quality = item.quality - item.quality
+    #             else:
+    #                 if item.quality < 50:
+    #                     item.quality = item.quality + 1
 
 
 class Item:
